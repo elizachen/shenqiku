27bcb1d1f2bbfea59928c218850e10c47b843347
elizachen <elizachen34@gmail.com>
2017-02-10 23:47:42 +0800

    Fix the image cannot compress bug.

--- config/initializers/carrierwave.rb
+++
@@ -1,6 +0,0 @@
-CarrierWave.configure do |config|
-    config.storage = :file
-    config.enable_processing = false
-    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
-    config.cache_dir      = Rails.root.join('tmp', 'uploads')
-end
