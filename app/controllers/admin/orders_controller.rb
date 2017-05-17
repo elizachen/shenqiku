a2462522087007c24e0552cccc15475edf848e8f
elizachen <elizachen34@gmail.com>
2017-05-18 07:05:55 +0800

    Try to show table, but failed

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,6 +6,25 @@ class Admin::OrdersController < ApplicationController
 
   def index
     @orders = Order.order("id DESC")
+
+    if @orders.any?
+     dates = (@orders.order("id ASC").first.created_at.to_date..Date.today).to_a
+     
+
+     @data3 = {
+       labels: dates,
+       datasets: Order::aasm_state.map do |s|
+         {
+           :label => I18n.t(s, :scope => "order.total"),
+           :data => dates.map{ |d|
+             @orders.total(s).where( "created_at >= ? AND created_at <= ?", d.beginning_of_day, d.end_of_day).count
+           },
+           borderColor: status_colors[s]
+         }
+       end
+     }
+   end
+
   end
 
   def show
